1fe34fcf4756f80793c960da7a71bbc200f03086
Ali Akrour <ali.akrour@gmail.com>
2020-11-29 16:21:32 +0100

    fine tuning apache & php conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,7 +40,16 @@ usermod -aG docker jenkins
 echo "*********************************************************"
 echo "* [7]: INSTALLATION DE APACHE, MYSQL, PHP, ET COMPOSER  *"
 echo "*********************************************************"
-apt-get install -y apache2 mysql-server php7.4 php7.4-bcmath php7.4-cli php7.4-curl php7.4-zip php7.4-sqlite3 php7.4-mysql php7.4-xml php7.4-mbstring php7.4-gd php-imagick php7.4-xsl php7.4-intl 
+apt-get install -y apache2 mysql-server php7.4 php7.4-bcmath php7.4-cli php7.4-curl php7.4-zip php7.4-sqlite3 php7.4-mysql php7.4-xml php7.4-mbstring php7.4-gd php-imagick php7.4-xsl php7.4-intl php-memcached
+sed -i 's/post_max_size = 8M/post_max_size = 128M/g' /etc/php/7.4/apache2/php.ini
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 128M/g' /etc/php/7.4/apache2/php.ini
+sed -i 's/;max_input_vars = 1000/max_input_vars = 5000/g' /etc/php/7.4/apache2/php.ini
+sed -i 's/memory_limit = 128M/memory_limit = 256M/g' /etc/php/7.4/apache2/php.ini
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+a2enmod rewrite
+source /etc/apache2/envvars
+systemctl restart apache2
 wget https://getcomposer.org/composer.phar
 mv composer.phar /usr/bin/composer
 chmod +x /usr/bin/composer
