ef4a3578c923e9a29ab7f26696e9e1f37428d990
Ali Akrour <ali.akrour@gmail.com>
2020-12-28 15:18:22 +0100

    Delete auto_cd.sh

--- auto_cd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-[[ ! `grep "cd /home/vagrant/PrestaShop" /home/vagrant/.bashrc` ]] && echo "cd /home/vagrant/PrestaShop" >> /home/vagrant/.bashrc
-# LA LIGNE CI-DESSUS DOIT ETRE SUPPRIMEE
-# LA LIGNE CI-DESSOUS DOIT ETRE DECOMMENTEE ( SUPPRIMER LA # et L'ESPACE AU DEBUT DE LA LIGNE )
-# [[ ! `grep "cd /vagrant" /home/vagrant/.bashrc` ]] && echo "cd /vagrant" >> /home/vagrant/.bashrc
-
